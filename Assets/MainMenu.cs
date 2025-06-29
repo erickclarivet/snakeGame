a42b51c9f5e36761acbd1c55ef18c97ce03e4321
erickclarivet <erick.clarivet@gmail.com>
2025-06-29 16:53:19 +0200

    first commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Launching game !");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ Assets/SnakeBehaviour.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+/* 
+    TODO:
+    - get user input v
+    - create apple (make it disapear) v
+    - create snake
+      * move v
+      * grow v
+      * collision (body or wall) => bug => fixed v 
+      * eat apple v
+      * add eyes to head 
+    - add walls v
+    - score v
+    - menu v
+    - music (Menu + Jeu) v
+    - Mettre des sprites (pommes, murs, serpent) v
+    - rotation tete v
+    - fluidifier deplacement
+    - Animations
+    - Best score
+    - lumieres
+    - audio/sound for the game (when click on buttons, when eat apple (gnom gnom gnom)) 
+    - pomme differents (pouvoir, cerises, raisins ect)
+    - subversion
+    - refacto
+    - how to export and multiple platform
+*/
+
+public class SnakeBehaviour : MonoBehaviour
+{
+    private Vector2 direction = Vector2.right;
+    private GameObject snakeComponent;
+    public GameObject foodComponent;
+    public GameObject bodyPartPrefab;
+    List<Transform> bodyParts = new List<Transform>();
+    List<Vector3> previousPositions = new List<Vector3>();
+
+    public float moveRate; // speed move
+    public float timer;
+    public TextMeshProUGUI scoreText;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveRate = 0.2f;
+        score = -1;
+        timer = 0;
+        UpdateScore();
+        snakeComponent = this.gameObject;
+        snakeComponent.transform.position = new Vector3(-9, 0, 0);
+        SpawnFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        timer += Time.deltaTime;
+
+        if (timer > moveRate)
+        {
+                    Debug.Log($"{timer}|{moveRate}");
+            timer = 0;
+            Move();
+        } 
+    }
+
+// Custom methods
+    void Move()
+    {
+        previousPositions.Insert(0, snakeComponent.transform.position);
+        snakeComponent.transform.position += new Vector3(direction.x, direction.y, 0);
+        // move parts :
+        for (int i = 0; i < bodyParts.Count; i++)
+        {
+            bodyParts[i].position = previousPositions[i];
+        }
+        // delete las position
+        if (previousPositions.Count > bodyParts.Count + 1)
+        {
+            previousPositions.RemoveAt(previousPositions.Count - 1);
+        }
+    }
+
+    void SpawnFood()
+    {
+        // Initiation random position
+        Vector2 position = new Vector2(Random.Range(-18, 18),Random.Range(-9, 9));
+        Instantiate(foodComponent, position, Quaternion.identity);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            Destroy(other.gameObject);
+            UpdateScore();
+            SpawnFood();
+            Grow();
+        }
+        else if (other.CompareTag("Body") || other.CompareTag("Wall"))
+        {
+            Debug.Log("GAME OVER!!");
+            Time.timeScale = 0f;
+            // not good
+        }
+    }
+
+    void Grow()
+    {
+        Vector3 newPosition = snakeComponent.transform.position;
+        previousPositions.Insert(0, snakeComponent.transform.position);
+        snakeComponent.transform.position += new Vector3(direction.x, direction.y, 0);
+        GameObject newPart = Instantiate(bodyPartPrefab, newPosition, Quaternion.identity);
+        bodyParts.Add(newPart.transform);
+    }
+
+    void UpdateScore()
+    {
+        score++;
+        scoreText.text = $"Score : {score}";
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && direction != Vector2.down)
+        {
+            direction = Vector2.up;
+            Rotate(0);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && direction != Vector2.up)
+        {
+            direction = Vector2.down;
+            Rotate(180);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && direction != Vector2.left)
+        {
+            direction = Vector2.right;
+            Rotate(-90);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && direction != Vector2.right)
+        {
+            direction = Vector2.left;
+            Rotate(90);
+        }
+    }
+
+    void Rotate(float angle)
+    {
+        snakeComponent.transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
